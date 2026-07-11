e54e5549ce2a2b5a8e5a8c35886fc21a04b2c823
Jojo <jsekyema1@guilford.edu>
2026-07-11 16:01:25 -0400

    Added basic odometry

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SmootMecanumOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SmootMecanumOpMode.java
@@ -1,36 +1,17 @@
 package org.firstinspires.ftc.teamcode;
 
-import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Range;
 
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.util.ElapsedTime;
-import com.qualcomm.robotcore.util.Range;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.Pose2D;
 
-
-/*
- * This file contains an minimal example of a Linear "OpMode". An OpMode is a 'program' that runs in either
- * the autonomous or the teleop period of an FTC match. The names of OpModes appear on the menu
- * of the FTC Driver Station. When a selection is made from the menu, the corresponding OpMode
- * class is instantiated on the Robot Controller and executed.
- *
- * This particular OpMode just executes a basic Tank Drive Teleop for a two wheeled robot
- * It includes all the skeletal structure that all linear OpModes contain.
- *
- * Use Android Studio to Copy this Class, and Paste it into your team's code folder with a new name.
- * Remove or comment out the @Disabled line to add this OpMode to the Driver Station OpMode list
- */
-
-@TeleOp(name="Basic: Linear OpMode", group="Linear OpMode")
-@Disabled
-public class BasicOpMode extends LinearOpMode {
+public class SmootMecanumOpMode extends LinearOpMode {
     // Declare OpMode members.
     private ElapsedTime runtime = new ElapsedTime();
 
@@ -39,11 +20,17 @@ public class BasicOpMode extends LinearOpMode {
     private DcMotorEx backleftMotor = null;
     private DcMotorEx frontrightMotor = null;
     private DcMotorEx frontleftMotor = null;
+    private GoBildaPinpointDriver odo = null;
 
     @Override
     public void runOpMode() {
         telemetry.addData("Status", "Initialized");
         telemetry.update();
+
+        odo = hardwareMap.get(GoBildaPinpointDriver.class, "odo");
+
+        odo.setOffsets(0.0, 0.0, DistanceUnit.MM);
+        odo.resetPosAndIMU();
 
         // Initialize the hardware variables. Note that the strings used here as parameters
         // to 'get' must correspond to the names assigned during the robot configuration
@@ -67,6 +54,8 @@ public class BasicOpMode extends LinearOpMode {
 
         // run until the end of the match (driver presses STOP)
         while (opModeIsActive()) {
+
+            Pose2D pos = odo.getPosition();
 
             // Setup a variable for each drive wheel to save power level for telemetry
             double brPower;
@@ -93,6 +82,7 @@ public class BasicOpMode extends LinearOpMode {
             telemetry.addData("Status", "Run Time: " + runtime.toString());
             telemetry.addData("Front Motors", "FL (%.2f), FR (%.2f)", flPower, frPower);
             telemetry.addData("Back Motors", "BL (%.2f), BR (%.2f)", blPower, brPower);
+            telemetry.addData("Odometry", "X: %.2f, Y: %.2f, H: %.2f", pos.getX(DistanceUnit.MM), pos.getY(DistanceUnit.MM), pos.getHeading(AngleUnit.DEGREES));
             telemetry.update();
         }
     }
